9fa1b576c467fa5d49f2ca99ef53c5560cc39feb
LeoInCode <lsm.ccomp@gmail.com>
2020-06-29 23:59:33 -0300

    Layout tela de login

--- src/database/insert.js
+++ src/database/insert.js
@@ -3,16 +3,20 @@ const db = require('./db')
 async function insertData(){
     await db.connect()
 
+    const nome = document.getElementById("inputUsuario").value
+    const email = document.getElementById("inputEmail").value
+    const cpf = document.getElementById("inputCpf").value
+    const rg = document.getElementById("inputRg").value
+    console.log(nome)
+    //const senha = document.getElementById("inputSenha").value
     
-    const queryAluno = "INSERT INTO aluno (cpf,rg,nome) VALUES ($1, $2, $3)"
+    const queryAluno = "INSERT INTO aluno (nome,email,cpf,rg) VALUES ($1, $2, $3, $4)"
 
-    await db.query(queryAluno,[87987,234234,'Leo'])
-    await db.query(queryAluno, [324234,56756,'Luana'])
+    await db.query(queryAluno,[nome,email,cpf,rg])
 
     await db.end()
 
-
-    console.log("Dados Inseridos");
+    console.log("Dados Inseridos")
 
 }
 insertData()
